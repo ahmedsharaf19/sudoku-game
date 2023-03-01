3f1b7fc9c71a60f6226f81e731661ae63837b843
Ahmed Sharaf <113258822+ahmedsharaf19@users.noreply.github.com>
2023-03-01 12:37:52 +0200

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -1,51 +1,63 @@
 import sudoku
-
-
-def write_board(board, choise_uniq_board, flag, filename):
-    """ Write Board With Generate Puzzle """
-
-    # flag knows me to choose the board
-    if flag:
-        board = sudoku.remove_field(board, choise_uniq_board)
-
-    # We will open the file in which we will write
-    with open(__file__.replace("file_operations.py", f"{filename}.txt"), "w") as f:
+import time
+def write_board (board,flag,filename) :
+    """ 
+    Write Board With Generate Puzzle 
+    parameter => board and choise write puzzle or solution and filname (solution or puzzle name)
+    return => void
+    """
+    # flag knows me to choose the board 
+    if flag :
+          board = sudoku.remove_field(board)
+     # We will open the file in which we will write
+    with open(__file__.replace("file_operations.py",f"{filename}.txt"), "w") as f:
         for i in range(9):
-            f.write(
-                " --------- --------- ---------  --------- --------- ---------  --------- --------- --------- \n")
-            f.write(
-                "|         |         |         ||         |         |         ||         |         |         |\n"*2)
+            f.write(" --------- --------- ---------  --------- --------- ---------  --------- --------- --------- \n")
+            f.write("|         |         |         ||         |         |         ||         |         |         |\n"*2)
             for j in range(9):
                 f.write("|")
-                if j == 3 or j == 6:
-                    f.write("|")
-                if board[i][j] != 0:
-                    f.write(f"    {board[i][j]}    ")
-                else:
-                    f.write("         ")
-                if j == 8:
-                    f.write("|")
-            f.write(
-                "\n|         |         |         ||         |         |         ||         |         |         |"*2)
-            if i == 2 or i == 5:
-                f.write(
-                    "\n --------- --------- ---------  --------- --------- ---------  --------- --------- --------- ")
+                if j == 3 or j == 6 :
+                     f.write("|")
+                if board[i][j] != 0 :
+                     f.write(f"    {board[i][j]}    ")
+                else :
+                     f.write("         ")
+                if j == 8 :
+                     f.write("|")
+            f.write("\n|         |         |         ||         |         |         ||         |         |         |"*2)
+            if i == 2 or i == 5 :
+                 f.write("\n --------- --------- ---------  --------- --------- ---------  --------- --------- --------- ")
             f.write("\n")
         f.write(" --------- --------- ---------  --------- --------- ---------  --------- --------- --------- \n\n\n")
-
-    print(__file__.replace("file_operations.py", f"{filename}.txt"))
-
+    print(__file__.replace("file_operations.py",f"{filename}.txt"))
 
 # generate complete board
 board = sudoku.generate_board()
-# copy orignal board before creating puzzle
+# copy orignal board before creating puzzle 
 solution_board = [[j for j in i] for i in board]
-# Ask users choise unique board or no
-choise_uniq_board = input(
-    "Are You Need Unique Board ('y' to yes and 'n' to 'No) : ").strip().lower()
 # generate puzzle board and write in file
-write_board(board, choise_uniq_board, True, 'sudoku_game')
+write_board(board,True,'sudoku_game')
+print("You Can Play Sudoku Game Now")
+start_time = time.time()
+choose = input("If You Finish Game Enter (y or yes) : ").strip().lower()
+if choose == 'y' or choose == 'yes' :
+     end_time = time.time()
+
 # Ask Users To Save Original Solution
-ch = input("You Need To Show Solution(y or n ) : ").strip().lower()
-if ch == 'y' or ch == 'yes':
-    write_board(solution_board, choise_uniq_board, False, 'solution')
+while True :
+     try :
+          ch = input("You Need To Show Solution(y or n ) : ").strip().lower()
+          if ch == 'y' or ch == 'yes' :
+               write_board(solution_board,False,' solution')
+               break
+          elif ch == 'n'  or ch == 'no' :
+               break
+          else :
+               raise ValueError
+     except ValueError :
+          print("Plese Enter Valid Choise!")
+     except KeyboardInterrupt:
+          print("\nGood Bye!")
+          exit()
+time_taken = end_time - start_time
+print(f"Time taken: {time_taken // 60:.0f} minutes {time_taken%60:.0f} Seconds ")
